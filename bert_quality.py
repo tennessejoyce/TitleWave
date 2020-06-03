d6eaefd7ffeebd3d3947450b9cdf73e65125a0cf
tennessejoyce <47008993+tennessejoyce@users.noreply.github.com>
2020-06-03 11:18:10 -0600

    Add files via upload

--- bert_quality.py
+++ bert_quality.py
@@ -3,38 +3,21 @@ import numpy as np
 import ktrain
 from ktrain import text
 from sklearn.model_selection import train_test_split
+
+#Titles are stored in this csv file.
+#I'm reading them out into a list of strings
 filename = 'physics.csv'
 df = pd.read_csv(directory+'/physics.csv')
 
-question_mask = (~df['@Title'].isna())&(df['@ClosedDate'].isna())
-titles = df['@Title'][question_mask].values
-#bodies = df['@Body'][question_mask].values
-
-
-def preprocess(s):
-	#Remove certain special characters, replace with spaces.
-	remove_list = ['\n','<p>','</p>']
-	for r in remove_list:
-		s = s.replace(r,' ')
-	return s
-
+#Select only the entries that are questions (not answers) and weren't closed.
 mask = (~df['@Title'].isna())&(df['@ClosedDate'].isna())
 titles = df['@Title'][mask].values
-#bodies = df['@Body'][mask].map(preprocess).values
-#scores = df['@Score'][mask].values
-#corr_score = np.log10(np.maximum(scores,1))
-#views = df['@ViewCount'][mask].values
-#answered = ~(df['@AcceptedAnswerId'][mask].isna())
 answered = df['@AnswerCount'][mask].values>=1
 
-#Take only the most recent
-titles = titles
-answered = answered
+#Split into train and test sets.
+x_train, x_test, y_train, y_test = train_test_split(titles, answered, test_size=0.2, random_state=42)
 
-x_train, x_test, y_train, y_test = train_test_split(titles, answered, test_size=0.2, random_state=41)
-
-			
-
+#Initialize the BERT model.
 class_names = ['unanswered','answered']
 MODEL_NAME = 'distilbert-base-uncased'
 t = text.Transformer(MODEL_NAME, maxlen=500, class_names=class_names)
@@ -43,18 +26,28 @@ val = t.preprocess_test(x_test, y_test)
 model = t.get_classifier()
 learner = ktrain.get_learner(model, train_data=trn, val_data=val, batch_size=6)
 
-#Fine tune the model. After 2 epochs, it seems to overfit.
+#Fine tune the model. After 2 epochs, it seems to overfit for me, but that probably depends on the task.
 learner.fit_onecycle(3e-5, 2)
 
+#Get a separate object to make predictions.
 predictor = ktrain.get_predictor(learner.model, preproc=t)
+#Save it to a file so we can use it elsewhere to make predictions.
 predictor.save('ktrain_predictor')
 
+#Predict the probabilities of getting an answer on the test set.
 y_pred = predictor.predict_proba(x_test)[:,1]
 
+#Compute the ROC-AUC score (a good metric for imbalanced classification).
 ra = roc_auc_score(y_test,y_pred)
 print(f'Test ROC-AUC: {ra}')
 
+#Separate the titles into quantiles (1 to 5 stars) based on the predicted probabilities,
+#then compute the actual probability of an answer within each quantile.
 divisions = [np.quantile(y_pred,q) for q in [0,0.2,0.4,0.6,0.8,1]]
 for i in range(5):
   qmask = (y_pred > divisions[i])&(y_pred <= divisions[i+1])
   print(f'{i+1} stars: {np.mean(y_test[qmask]):.3f}   {np.sum(qmask)}')
+
+
+#To understand which words are contributing to the prediction.
+#predictor.explain()
