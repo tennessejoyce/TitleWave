b09b3838cc7e9588a2f0b5c8f9a80a99d59a959e
tennessejoyce <tuckerj229@gmail.com>
2021-02-06 16:25:06 -0500

    tensorflowjs conversion failed

--- model_training/bash_scripts/download_xml.sh
+++ model_training/bash_scripts/download_xml.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-#First command line argument is the name of the Stack Exchange forum.
-
-# Make the directory to hold the data for that forum.
-if [ ! -d "$1" ]; then
-  mkdir $1
-fi
-
-cd $1
-
-dataset_dir="dataset"
+# First command line argument is the name of the Stack Exchange forum.
+forum="$1"
 
 # Download and extract the dataset from archive.org.
-if [ $1 == "overflow" ]; then
+if [ $forum == "overflow" ]; then
   # Special naming convention for StackOverflow, the biggest forum.
-  if [ ! -d "$dataset_dir" ]; then
-    mkdir "$dataset_dir"
-  fi
-  cd "$dataset_dir"
   if [ ! -f "Posts.7z" ]; then
     wget -O "Posts.7z" "https://archive.org/download/stackexchange/stackoverflow.com-Posts.7z"
   fi
@@ -25,13 +13,14 @@ if [ $1 == "overflow" ]; then
     7z e "Posts.7z"
   fi
 else
-  if [ ! -f "$dataset_dir.7z" ]; then
-    wget -O "$dataset_dir.7z" "https://archive.org/download/stackexchange/$1.stackexchange.com.7z"
+  if [ ! -f "$forum.7z" ]; then
+    wget -O "$forum.7z" "https://archive.org/download/stackexchange/$forum.stackexchange.com.7z"
   fi
-  if [ ! -d "$dataset_dir" ]; then
-    7z e "$dataset_dir.7z" -o"$dataset_dir"
+  if [ ! -d "$forum" ]; then
+    7z e "$forum.7z" -o"$forum"
   fi
 fi
+
 # Start the MongoDB service
 if ! systemctl is-active --quiet mongod; then
   echo "Starting Mongo..."
--- model_training/bash_scripts/export_model.sh
+++ model_training/bash_scripts/export_model.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-cd data/BERT_training
+# Confirm that we're using the right python interpreter (with tensorflowjs)
+which python
+
+cd data/BERT
 pytorch_model_loc="BERT_pytorch_model"
 keras_model_loc="BERT_keras_model.h5"
 tfjs_model_loc="BERT_tfjs_model"
-python ../../model_training/python_scripts/pytorch_to_keras.py "$pytorch_model_loc" "$keras_model_loc"
-echo "Successfully converted from Pytorch to Keras h5..."
+if [ ! -f "$keras_model_loc" ]; then
+  echo "Converting from Pytorch to Keras h5..."
+  python ../../model_training/python_scripts/pytorch_to_keras.py "$pytorch_model_loc" "$keras_model_loc"
+fi
+echo "Converting from keras to tensorflowjs..."
 #tensorflowjs_converter --input_format keras "$keras_model_loc" "$tfjs_model_loc"
